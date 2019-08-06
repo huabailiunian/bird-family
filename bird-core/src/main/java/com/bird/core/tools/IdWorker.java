208cc6b5b918296cd4cef24aa5b6c1430dbedc6a
bird <huabailiunian.@163.com>
2019-08-06 10:43:33 +0800

    update idWorker

--- bird-core/src/main/java/com/bird/core/tools/IdWorker.java
+++ bird-core/src/main/java/com/bird/core/tools/IdWorker.java
@@ -7,7 +7,7 @@ package com.bird.core.tools;
  * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0<br>
  * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
  * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69<br>
- * 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId<br>
+ * 10位的数据机器位，可以部署在1024个节点，包括5位centerId和5位workerId<br>
  * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号<br>
  * 加起来刚好64位，为一个Long型。<br>
  * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
@@ -25,12 +25,12 @@ public class IdWorker {
     /**
      * 机器id所占的位数
      */
-    private final long workerIdBits = 7L;
+    private final long workerIdBits = 6L;
 
     /**
      * 数据标识id所占的位数
      */
-    private final long centerIdBits = 3L;
+    private final long centerIdBits = 4L;
 
     /**
      * 支持的最大机器id，结果是127 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数)
@@ -164,13 +164,29 @@ public class IdWorker {
         return System.currentTimeMillis();
     }
 
+    public long getMaxWorkerId() {
+        return maxWorkerId;
+    }
+
+    public long getMaxCenterId() {
+        return maxCenterId;
+    }
+
+    public long getWorkerId() {
+        return workerId;
+    }
+
+    public long getCenterId() {
+        return centerId;
+    }
+
     //==============================Test=============================================
 
     /**
      * 测试
      */
-    public static void main(String[] args) {
-        IdWorker idWorker = new IdWorker(0, 0);
-        System.out.println(idWorker.nextId());
-    }
+//    public static void main(String[] args) {
+//        IdWorker idWorker = new IdWorker(0, 0);
+//        System.out.println(idWorker.nextId());
+//    }
 }
