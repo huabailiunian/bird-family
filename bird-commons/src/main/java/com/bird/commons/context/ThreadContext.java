7b81a40182469b6aaf4eec265c5b7c7acdd904bb
bird <34894356@qq.com>
2020-04-29 20:47:34 +0800

    state

---
+++ bird-commons/src/main/java/com/bird/commons/context/ThreadContext.java
@@ -0,0 +1,100 @@
+package com.bird.commons.context;
+
+import com.bird.commons.tools.CollectionTools;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Thread local context
+ * 线程变量上下文
+ *
+ * @author youly
+ * 2019/7/24 10:06
+ */
+public class ThreadContext {
+
+    private static ThreadLocal<Map<String, Object>> resources = new InheritableThreadLocalMap();
+
+    protected ThreadContext() {
+    }
+
+    public static Map<String, Object> getResource() {
+        return CollectionTools.isNotEmpty(resources.get()) ? new HashMap<>(resources.get()) : Collections.emptyMap();
+    }
+
+    public static void setResource(Map<String, Object> resource) {
+        if (CollectionTools.isEmpty(resource)) {
+            return;
+        }
+        ensureResourcesInitialized();
+        resources.get().clear();
+        resources.get().putAll(resource);
+    }
+
+    private static void ensureResourcesInitialized() {
+        if (resources.get() == null) {
+            resources.set(new HashMap<>(16));
+        }
+    }
+
+    public static Object get(String key) {
+        return getValue(key);
+    }
+
+    private static Object getValue(String key) {
+        Map<String, Object> preResources = resources.get();
+        return CollectionTools.isEmpty(preResources) ? null : preResources.get(key);
+    }
+
+    public static void put(String key, Object value) {
+        if (key == null) {
+            throw new IllegalArgumentException("key cannot be null");
+        }
+
+        if (value == null) {
+            remove(key);
+            return;
+        }
+        ensureResourcesInitialized();
+        resources.get().put(key, value);
+    }
+
+    public static Object remove(String key) {
+        Map<String, Object> preResources = resources.get();
+        return CollectionTools.isEmpty(preResources) ? null : preResources.remove(key);
+    }
+
+    /**
+     * {@link ThreadLocal#remove Remove}s the underlying {@link ThreadLocal ThreadLocal} from the thread.
+     * <p/>
+     * This method is meant to be the final 'clean up' operation that is called at the end of thread execution to
+     * prevent thread corruption in pooled thread environments.
+     *
+     * @since 1.0
+     */
+    public static void remove() {
+        resources.remove();
+    }
+
+    private static final class InheritableThreadLocalMap extends InheritableThreadLocal<Map<String, Object>> {
+
+        /**
+         * This implementation was added to address a
+         * user-reported issue</a>.
+         *
+         * @param parentValue the parent value, a HashMap as defined in the {@link #initialValue()} method.
+         * @return the HashMap to be used by any parent-spawned child threads (a clone of the parent HashMap).
+         */
+        @Override
+        @SuppressWarnings("unchecked")
+        protected Map<String, Object> childValue(Map<String, Object> parentValue) {
+            if (parentValue != null) {
+                return (Map<String, Object>) ((HashMap<String, Object>) parentValue).clone();
+            } else {
+                return null;
+            }
+        }
+    }
+}
--- bird-demo/src/test/java/com/bird/demo/BaseTest.java
+++ bird-demo/src/test/java/com/bird/demo/BaseTest.java
@@ -6,6 +6,29 @@ public class BaseTest {
 
     @Test
     public void name() {
-        System.out.println(Long.MAX_VALUE);
+        System.out.println(tableSizeFor(17));
+    }
+
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        System.out.println(n);
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 1;
+        System.out.println(n);
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 2;
+        System.out.println(n);
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 4;
+        System.out.println(n);
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 8;
+        System.out.println(n);
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 16;
+        System.out.println(n);
+        System.out.println(Integer.toBinaryString(n));
+        return (n < 0) ? 1 : (n >= (1<<30)) ? (1<<30) : n + 1;
     }
 }
