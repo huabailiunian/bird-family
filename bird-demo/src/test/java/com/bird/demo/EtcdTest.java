ed3e85ae81a18a3c6cfdc5639da5f0c74ab21021
master <34894356@qq.com>
2020-05-07 08:47:56 +0800

    update

--- bird-demo/src/test/java/com/bird/demo/EtcdTest.java
+++ bird-demo/src/test/java/com/bird/demo/EtcdTest.java
@@ -1,17 +1,9 @@
 package com.bird.demo;
 
-import com.bird.commons.constant.Const;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.etcd.jetcd.ByteSequence;
-import io.etcd.jetcd.Client;
-import io.etcd.jetcd.KV;
-import io.etcd.jetcd.KeyValue;
-import io.etcd.jetcd.kv.GetResponse;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -26,18 +18,18 @@ public class EtcdTest {
 
     @Test
     public void connTest() throws ExecutionException, InterruptedException, JsonProcessingException {
-        Client client = Client.builder().endpoints(url).build();
-        KV kvClient = client.getKVClient();
-        ByteSequence key = ByteSequence.from("name".getBytes(Const.CHARSET_UTF8));
-        ByteSequence value = ByteSequence.from("Bob".getBytes(Const.CHARSET_UTF8));
-//        kvClient.put(key,value).get();
-
-        CompletableFuture<GetResponse> future = kvClient.get(key);
-        List<KeyValue> values = future.get().getKvs();
-        values.stream().forEach(v -> {
-            byte[] bytes = v.getValue().getBytes();
-            System.out.println(new String(bytes, Const.CHARSET_UTF8));
-        });
+//        Client client = Client.builder().endpoints(url).build();
+//        KV kvClient = client.getKVClient();
+//        ByteSequence key = ByteSequence.from("name".getBytes(Const.CHARSET_UTF8));
+//        ByteSequence value = ByteSequence.from("Bob".getBytes(Const.CHARSET_UTF8));
+////        kvClient.put(key,value).get();
+//
+//        CompletableFuture<GetResponse> future = kvClient.get(key);
+//        List<KeyValue> values = future.get().getKvs();
+//        values.stream().forEach(v -> {
+//            byte[] bytes = v.getValue().getBytes();
+//            System.out.println(new String(bytes, Const.CHARSET_UTF8));
+//        });
 
     }
 }
